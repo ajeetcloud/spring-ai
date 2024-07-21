288e26b93cc809930382786b1b3aac79c3526d32
Ajeet Singh <ajeet.cloud@gmail.com>
2024-07-21 15:35:26 +0530

    Added streaming endpoint

--- src/main/java/com/example/springai/controller/BasicController.java
+++ src/main/java/com/example/springai/controller/BasicController.java
@@ -5,22 +5,22 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
-import org.springframework.ai.ollama.api.OllamaApi;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 import java.util.List;
 
 @RestController
-public class LLamaController {
+public class BasicController {
     private final ChatClient chatClient;
 
-    public LLamaController(ChatClient.Builder chatClientBuilder) {
+    public BasicController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
     }
 
@@ -43,5 +43,25 @@ public class LLamaController {
 
         return res.chatResponse().getResults();
     }
+
+    @PostMapping("/chatWithStreamingResponse")
+    public Flux<ChatResponse> chatWithStreamingResponse(@RequestBody QueryRequest request) {
+
+        Message userMessage = new UserMessage(request.getQuery());
+        Message systemMessage = new SystemMessage("You are an expert in Java. Please provide to the point and accurate answers.");
+        Prompt prompt = new Prompt(List.of(systemMessage, userMessage));
+
+        Flux<ChatResponse> flux = chatClient.prompt(prompt).stream().chatResponse();
+
+        flux.subscribe(
+          chatResponse -> System.out.print(chatResponse.getResult().getOutput().getContent()), // onNext
+          throwable -> System.err.println("Error occurred: " + throwable), // onError
+                () -> System.out.println("Streaming complete") // onComplete
+        );
+
+        return flux;
+    }
+
+
 
 }
